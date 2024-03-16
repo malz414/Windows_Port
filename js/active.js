45ed508d83c7fccd0a99ea8ffb42cfaa0da419bf
malz414 <72981910+malz414@users.noreply.github.com>
2024-03-16 23:27:30 +0000

    redid site as windows
    REDON

--- js/active.js
+++ js/active.js
@@ -6,11 +6,11 @@
     var browserWindow = $(window);
 
     // :: 1.0 Preloader Active Code
-    // browserWindow.on('load', function () {
-    //     $('.preloader').fadeOut('slow', function () {
-    //         $(this).remove();
-    //     });
-    // });
+     browserWindow.on('load', function () {
+         $('.preloader').fadeOut('slow', function () {
+             $(this).remove();
+         });
+     });
 
  
     // :: 1.0 Preloader Active Code
